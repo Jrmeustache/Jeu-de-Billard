1c5b3083faf79a19aa026e87ee34474cd2073cb9
ManuelPod <61513832+ManuelPod@users.noreply.github.com>
2021-02-19 11:13:45 -0500

    Camera

--- Billard/Assets/Scripts/CameraLook.cs
+++ Billard/Assets/Scripts/CameraLook.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float horizontalSpeed = 2f;
+    public float verticalSpeed = 2f;
+    public float height = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
+
 }
